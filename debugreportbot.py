8e1992184fae404309d65aff159cac06e0b6c66c
rain <tony@rainzy.net>
2018-01-17 16:29:32 +0100

    added bug reporting bot
    not yet working!

---
+++ debugreportbot.py
@@ -0,0 +1,16 @@
+while 1 == 1: #loop
+
+x = 0 #placeholder input from API
+y = chat.id
+ bn = #get from db
+fbot != false #msg from bot?
+bn = existing bugs #b1, b2, b3....
+if bn == x #bug known and fbot == true:
+-> r = true
+-> bn = bn + 1
+-> print("Report received, however I think that this issue is related to bug \#" + bn + ". I will notify developers of its occurance for confirmation")
+-> elif :
+-> ->  r = false
+-> -> print("Bug \#" + bn " reported successfully!")
+if error == true:
+break
